4a083543d7b2e188427cae96536bc962c8d7dcd0
gajagajago <gajagajago@naver.com>
2021-02-27 18:50:17 +0900

    add cart done

--- app/controllers/api/v1/cart_items_controller.rb
+++ app/controllers/api/v1/cart_items_controller.rb
@@ -1,9 +1,36 @@
 class Api::V1::CartItemsController < ApplicationController
-  def create
+  before_action :init_cart_item, only: :create
 
+  def create
+    puts @cartItem.nil?
+
+    if @cartItem.nil?
+      @cartItem = CartItem.new(cart_item_params)
+
+      if @cartItem.save!
+        render json: {msg: "Added new item to cart"}
+      end
+    else
+      @cartItem.update(product_count: @cartItem.product_count + params['product_count'])
+
+      render json: {msg: "Updated product count of cart item"}
+    end
   end
 
   def destroy
 
+  end
+
+  private
+  def cart_item_params
+    {
+      cart_id: params['cart_id'],
+      product_id: params['product_id'],
+      product_count: params['product_count'],
+    }
+  end
+
+  def init_cart_item
+    @cartItem = CartItem.find_by(cart_id: params['cart_id'], product_id: params['product_id'])
   end
 end
